d7cc2bd0e50418e1c5b11d5c026439fecd1f8af8
Erol Gelbul <53209203+ErolGelbul@users.noreply.github.com>
2022-03-15 20:06:59 +0000

    fixing self request

--- main.py
+++ main.py
@@ -2,10 +2,12 @@ import justpy as jp
 
 from webapp.about import About
 from webapp.home import Home
+from webapp.dictionary import Dictionary
 
 #Route only expects function path, no arguments
 jp.Route(About.path, About.serve)
 jp.Route(Home.path, Home.serve)
+jp.Route(Dictionary.path, Dictionary.serve)
 
 jp.justpy(port=8001)
 
